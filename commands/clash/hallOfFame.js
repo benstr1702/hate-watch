a2c14195c5c97ccacee84e99fcbb4f9830128073
benstr <66129129+benstr1702@users.noreply.github.com>
2025-12-03 10:26:42 +0200

    update hof ranking

--- commands/clash/hallOfFame.js
+++ commands/clash/hallOfFame.js
@@ -22,6 +22,11 @@ module.exports = {
 					name: "3. Remus knocked out by 0-5 tournament player",
 					value: "https://discord.com/channels/1258477556110200833/1258587331326382080/1440844177460957344",
 				},
+
+				{
+					name: "9. Sena struggles to get a single win in 10 games",
+					value: "https://discord.com/channels/1258477556110200833/1258587331326382080/1416633398067331274",
+				},
 				{
 					name: "4. Ace goes on a midnight walk to prepare mentally for his match",
 					value: "https://discord.com/channels/1258477556110200833/1258587331326382080/1440120089708986448",
@@ -29,22 +34,6 @@ module.exports = {
 				{
 					name: "5. bagshawties last minute sub",
 					value: "https://discord.com/channels/1258477556110200833/1258587331326382080/1439335631175159971",
-				},
-				{
-					name: "6. Remus sweating profusely as he gets his ass handed to him by Caca",
-					value: "https://discord.com/channels/1258477556110200833/1258587331326382080/1418234638354219183",
-				},
-				{
-					name: "7. Benis tournament final choke",
-					value: "https://discord.com/channels/1258477556110200833/1258587331326382080/1444062425493012715",
-				},
-				{
-					name: "8. Investigative journalist 'Peemus' EXPOSES Irish national 'Niggey' ",
-					value: "https://discord.com/channels/1258477556110200833/1258587331326382080/1426351409020014593",
-				},
-				{
-					name: "9. Sena struggles to get a single win in 10 games",
-					value: "https://discord.com/channels/1258477556110200833/1258587331326382080/1416633398067331274",
 				}
 			)
 			.setFooter({ text: "Clash Royale Hatewatch Bot Database" });
